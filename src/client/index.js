8c9182e794329698d021307beb149a75250194c2
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-19 14:52:51 +0300

    Fixes.

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-  DemoPageIndex: require('./components/DemoPageIndex.react.js').default
+  DemoPageIndex: require('./components/DemoPageIndex.react.js').default,
+  utils: {
+    parseComponents: require('./parseComponents').default
+  }
 };
