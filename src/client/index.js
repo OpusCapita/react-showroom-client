ce6c76881c16710243286ca275e1720b31ddbe56
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-26 14:17:37 +0300

    Fixed exports.

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,2 @@
-module.exports = {
-  DemoPageIndex: require('./components/DemoPageIndex.react.js').default,
-  utils: {
-    parseComponents: require('./parseComponents').default
-  }
-};
+module.exports = require('./components/DemoPageIndex.react.js').default;
+
