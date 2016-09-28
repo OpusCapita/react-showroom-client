03019fa21ae3cb2fca60eea6b0d6a204ad9e9215
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-28 18:39:48 +0300

    Improved publishing process.

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'lib'),
     publicPath: `http://${host}:${port}/`,
-    filename: `bundle.js`,
+    filename: `index.js`,
     library: 'demopage',
     libraryTarget: 'umd'
   },
