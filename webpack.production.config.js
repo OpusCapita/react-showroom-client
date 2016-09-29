214149175ae63e6c1d77b01843073d830f6597c7
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-29 09:35:42 +0300

    Fixed build.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,7 @@ let config = require('./webpack.development.config');
 config.entry = path.resolve(path.join(__dirname, './src/client/index.js'));
 delete config.devtool;
 config.externals = {
-  'react': 'React'
+  'react': 'react'
 };
 config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
