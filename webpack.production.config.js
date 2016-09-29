b32ec72e250af2a578e1729c3382000027822553
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-29 10:08:28 +0300

    Fixed build.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,18 @@ let config = require('./webpack.development.config');
 config.entry = path.resolve(path.join(__dirname, './src/client/index.js'));
 delete config.devtool;
 config.externals = {
-  'react': 'React'
+  react: {
+    root: 'React',
+    commonjs2: 'react',
+    commonjs: 'react',
+    amd: 'react'
+  },
+  'react-dom': {
+    root: 'ReactDOM',
+    commonjs2: 'react-dom',
+    commonjs: 'react-dom',
+    amd: 'react-dom'
+  }
 };
 config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
