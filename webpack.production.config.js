1471e303ac443dc3f2dec42230fc898f1cf3da4f
volkovich <Kirill.Volkovich@jCatalog.com>
2016-09-29 09:10:34 +0300

    Fixed build.

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 let config = require('./webpack.development.config');
+config.entry = path.resolve(path.join(__dirname, './src/client/index.js'));
 delete config.devtool;
 config.plugins = config.plugins.concat([
   new webpack.DefinePlugin({
