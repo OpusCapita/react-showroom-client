ef3572339b6afce6df45b9ad2e5991ec60feca1d
volkovich <Kirill.Volkovich@jCatalog.com>
2016-10-19 11:42:52 +0300

    Added documenting comment to showroomScopeDecorator.

--- src/client/decorators/showroomScopeDecorator.js
+++ src/client/decorators/showroomScopeDecorator.js
@@ -12,6 +12,11 @@ target => {
     } catch (err) {
       console.log('Showroom scope decorator - transpiling code error:', err);
     }
+
+    /* Use '_scope' keyword in DOCUMENTATION file when want get access to SCOPE component.
+       For example - '<ContainerWithControls showModal={ _scope.state.showModal } />'
+       For code 'this.state.a' After transpiling finish, result code is 'undefined.state.a'.
+       Because 'undefined' is an frequently occurring word, we use '_scope' */
     transpiledCode = transpiledCode.replace(/_scope/g, 'this');
     let compiledCode = eval(transpiledCode); // eslint-disable-line no-eval
     return compiledCode;
