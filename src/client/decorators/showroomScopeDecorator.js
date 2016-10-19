10f094ad9dcdd2307815bf294ba48ad9bf166ea2
volkovich <Kirill.Volkovich@jCatalog.com>
2016-10-19 11:58:27 +0300

    Changed documenting comment to showroomScopeDecorator.

--- src/client/decorators/showroomScopeDecorator.js
+++ src/client/decorators/showroomScopeDecorator.js
@@ -14,7 +14,7 @@ target => {
     }
 
     /* Use '_scope' keyword in DOCUMENTATION file when want get access to SCOPE component.
-       For example - '<ContainerWithControls showModal={ _scope.state.showModal } />'
+       For example - '<ModalContainer showModal={ _scope.state.showModal } />'
        For code 'this.state.a' After transpiling finish, result code is 'undefined.state.a'.
        Because 'undefined' is an frequently occurring word, we use '_scope' */
     transpiledCode = transpiledCode.replace(/_scope/g, 'this');
