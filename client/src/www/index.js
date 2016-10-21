b33ac7cad45ca98a54cba72d1d17737e4df503b9
volkovich <Kirill.Volkovich@jCatalog.com>
2016-10-21 20:31:56 +0300

    Server preconfigured.

--- client/src/www/index.js
+++ client/src/www/index.js
@@ -2,18 +2,36 @@
 
 const path = require('path');
 const express = require('express');
+const webpack = require('webpack');
+const webpackMiddleware = require('webpack-dev-middleware');
 const app = express();
-const host = require('../../serverConfig').host;
-const port = require('../../serverConfig').port;
+const host = require('../../../commons/serverConfig').host;
+const port = require('../../../commons/serverConfig').port;
 
-const npmLoader = require('../tools/npm-installer/loader');
-const npmInstallerConfig = require('../tools/npm-installer/config');
-const npmScannerConfig = require('../tools/npm-scanner/config');
+const npmLoader = require('./tools/npm-installer/loader');
+const npmInstallerConfig = require('./tools/npm-installer/config');
+const npmScannerConfig = require('./tools/npm-scanner/config');
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+});
+
+app.use(express.static(path.normalize(__dirname + '/../lib/')));
+app.use(webpackMiddleware(webpack(require('../../client/webpack.development.config.js')), {
+  publicPath: '/',
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: true
+  },
+  stats: {
+    colors: true
+  }
+}));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.normalize(__dirname + '/index.html'));
 });
 
 app.get('/packages-info', function(req, res) {
