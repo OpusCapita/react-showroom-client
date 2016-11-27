95db15058997e8d9a637ffa980e45acf871ed837
volkovich <Kirill.Volkovich@jCatalog.com>
2016-11-27 15:34:20 +0300

    Fixed i18n manager in default scope component.

--- src/components/DefaultScopeComponent/DefaultScopeComponent.react.js
+++ src/components/DefaultScopeComponent/DefaultScopeComponent.react.js
@@ -1,15 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import { I18nManager } from 'jcatalog-i18n';
+import { I18nManager } from '@opuscapita/i18n';
 import showroomScopeDecorator from '../../decorators/showroomScopeDecorator';
 
 const formatPatterns = {
-  'en': {
-    'datePattern': 'MM/dd/yyyy',
-    'integerPattern': '#,##0',
-    'numberPattern': '#,##0.00',
-    'numberDecimalSeparator': '.',
-    'numberGroupingSeparator': ',',
-    'numberGroupingSeparatorUse': true
+  en: {
+    datePattern: 'MM/dd/yyyy',
+    integerPattern: '#,##0',
+    numberPattern: '#,##0.00',
+    numberDecimalSeparator: '.',
+    numberGroupingSeparator: ',',
+    numberGroupingSeparatorUse: true
   },
   de: {
     datePattern: "dd.MM.yyyy",
