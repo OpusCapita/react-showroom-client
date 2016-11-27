ac95768c0a2d22c7fb8420d043e09c0068b3930e
volkovich <Kirill.Volkovich@jCatalog.com>
2016-11-27 19:16:24 +0300

    Removed 'build' npm-goal. Updated '@opuscapita/i18n' for not throwing error when some components require i18n in context. Integrated '@opuscapita/npm-scripts'.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,3 @@ import Showroom from './components/DemoPageIndex';
 export const showroomScopeDecorator = require('./decorators/showroomScopeDecorator').default;
 export const remoteLoader = require('./remote-loader/index').default;
 export default Showroom;
-
