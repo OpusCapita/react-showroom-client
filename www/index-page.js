14a2a4fa1cbfb4133daf3c6f7faf46e0fa009334
Kirill Volkovich <visortelle@gmail.com>
2017-03-02 21:12:53 +0300

    Changed ReactDOM root

--- www/index-page.js
+++ www/index-page.js
@@ -5,7 +5,7 @@ import { remoteLoader } from '../src/index';
 import '../external_modules/jcatalog-bootstrap/dist/less/jcatalog-bootstrap-bundle.less';
 import '../external_modules/jcatalog-bootstrap/dist/less/jcatalog-bootstrap-extensions-bundle.less';
 
-let rootElement = document.getElementById('main');
+let rootElement = document.getElementById('showroom-root');
 let demoPageIndex = React.createElement(DemoPageIndex, { loader: remoteLoader });
 
 ReactDOM.render(demoPageIndex, rootElement);
